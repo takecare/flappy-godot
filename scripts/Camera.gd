7eb72036b286ad3448d22908a0a9809b398098c3
takecare <rui@vazteixeira.org>
2020-05-05 09:04:36 +0100

    load player from path or automatically

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,14 +1,18 @@
 extends Camera2D
 
 export(float) var HORIZONTAL_OFFSET = 0.25
-export(NodePath) var player = null
+export(NodePath) var playerPath = null
+
+var player
 
 func _ready() -> void:
   var screensize = get_viewport().get_visible_rect().size
   offset.x = -screensize.x * HORIZONTAL_OFFSET
-  player = get_node("../Bird")
+  if playerPath == null:
+    player = get_tree().get_root().get_child(0).get_node("Bird")
+  else:
+    player = get_node(playerPath)
 
 # warning-ignore:unused_argument
 func _process(delta: float) -> void:
   position = Vector2(player.position.x, position.y)
-  pass

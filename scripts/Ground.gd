b87b1925c1b8b7a23c50680f19152eb7b202fe93
takecare <rui@vazteixeira.org>
2020-05-08 09:14:29 +0100

    fix ground spawning pool

--- scripts/Ground.gd
+++ scripts/Ground.gd
@@ -3,3 +3,6 @@ extends StaticBody2D
 class_name Ground
 
 onready var rightmostPoint = get_node("Rightmost").get_global_position()
+
+func get_rightmost_point(): 
+    return get_node("Rightmost").get_global_position()
--- scripts/GroundSpawner.gd
+++ scripts/GroundSpawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-# const groundScene = preload("res://scenes/Ground.tscn")
+const NUM_OF_GROUND_INSTANCES = 3
+
 export(NodePath) var containerPath = null
 export(NodePath) var cameraPath = null
 export(Resource) var groundScene = preload("res://scenes/Ground.tscn")
@@ -13,42 +14,33 @@ var camera: Node2D = null
 func _ready() -> void:
   position.y = Utils.get_screen_size().y
   nextPosition = position
-
-  # container = Utils.get_path_or_node("Container", containerPath)
   if containerPath == null:
     container = get_node("Container")
   else:
     container = get_node(containerPath)
-
   if cameraPath == null:
     camera = Utils.get_main_node().get_node("Camera")
   else:
     camera = get_node(cameraPath)
-
-  for i in range(3):
+  for i in range(NUM_OF_GROUND_INSTANCES):
     pool.append(instantiateGroundAt(i))
-  # spawn()
 
-func instantiateGroundAt(pos: int):
-  # var ground = groundScene.instance()
-  # ground.position.x = position
+func instantiateGroundAt(pos: int) -> Node:
   var newGround = groundScene.instance()
-  var node = newGround.get_node("Sprite")
+  var node = newGround.get_node("Sprite") # bad: tightly coupled to the ground scene's internals
   newGround.position.x = node.texture.get_size().x * pos
   container.add_child(newGround)
   return newGround
 
-func spawn():
-  var newGround = groundScene.instance()
-  # newGround.position = nextPosition
-  var node = newGround.get_node("Sprite")
-  nextPosition.x = node.texture.get_size().x
-  container.add_child(newGround)
-
 func _process(_delta: float) -> void:
+  if camera == null:
+    return
   updateGroundInstances()
 
+var lastMoved = pool.size() - 1
+
 func updateGroundInstances() -> void:
-  for ground in pool:
-    if ground.rightmostPoint.x < camera.get_correct_position().x:
-      ground.position.x = pool[pool.size() - 1].rightmostPoint.x
+  for i in range(0, pool.size()):
+    if pool[i].get_rightmost_point().x < camera.get_correct_position().x:
+      pool[i].position.x = pool[lastMoved].get_rightmost_point().x
+      lastMoved = i

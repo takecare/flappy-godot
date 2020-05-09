80c6f561399ffe9c6740b14dd0dc7c305ad816f2
takecare <rui@vazteixeira.org>
2020-05-09 11:16:37 +0100

    add pipespawner

--- scripts/GroundSpawner.gd
+++ scripts/GroundSpawner.gd
@@ -8,8 +8,8 @@ export(Resource) var groundScene = preload("res://scenes/Ground.tscn")
 
 var nextPosition: Vector2 = Vector2()
 var container: Node = null
-var pool: Array = []
-var camera: Node2D = null
+var groundPool: Array = []
+var camera: Camera2D = null
 
 func _ready() -> void:
   position.y = Utils.get_screen_size().y
@@ -23,9 +23,9 @@ func _ready() -> void:
   else:
     camera = get_node(cameraPath)
   for i in range(NUM_OF_GROUND_INSTANCES):
-    pool.append(instantiateGroundAt(i))
+    groundPool.append(instantiateGroundAt(i))
 
-func instantiateGroundAt(pos: int) -> Node:
+func instantiateGroundAt(pos: int) -> Node2D:
   var newGround = groundScene.instance()
   var node = newGround.get_node("Sprite") # bad: tightly coupled to the ground scene's internals
   newGround.position.x = node.texture.get_size().x * pos
@@ -37,10 +37,13 @@ func _process(_delta: float) -> void:
     return
   updateGroundInstances()
 
-var lastMoved = pool.size() - 1
+var lastMoved = groundPool.size() - 1
 
 func updateGroundInstances() -> void:
-  for i in range(0, pool.size()):
-    if pool[i].get_rightmost_point().x < camera.get_correct_position().x:
-      pool[i].position.x = pool[lastMoved].get_rightmost_point().x
+  for i in range(0, groundPool.size()):
+    if cameraIsPastGround(groundPool[i]):
+      groundPool[i].position.x = groundPool[lastMoved].get_rightmost_point().x
       lastMoved = i
+
+func cameraIsPastGround(ground: Node2D) -> bool:
+  return ground.get_rightmost_point().x < camera.get_correct_position().x
---
+++ scripts/PipeSpawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+export(NodePath) var cameraPath = null
+export(NodePath) var containerPath = null
+export(Resource) var pipeScene = preload("res://scenes/Pipes.tscn")
+
+onready var container: Node = containerPath if noget_node("Container")
+
+func _ready() -> void:
+  spawnPipe()
+  pass
+
+func _process(_delta: float) -> void:
+  pass
+
+func spawnPipe() -> void:
+  var newPipes = pipeScene.instance()
+  newPipes.position = position
+  container.add_child(newPipes)
+  pass
+
+func moveToNextSpawnPoint() -> void:
+  pass

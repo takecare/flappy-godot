dedbf5326f72e5c57129e9841096a66a47c5918c
takecare <rui@vazteixeira.org>
2020-05-17 18:45:51 +0100

    display current score

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,21 +1,26 @@
 extends Node
 
+signal score_changed
+signal best_score_changed
+
 const birdGroup = "birdGroup"
 const pipeGroup = "pipes"
 const groundGroup = "grounds"
 
-var score: int = 0 setget set_score,get_score
+var score: int = 0 setget set_score
+var bestScore: int = 0 setget set_score
 
 func _ready() -> void:
     pass
 
 func increase_score() -> void:
     score = score + 1
-    print("score: " + str(score))
+    emit_signal("score_changed", score)
 
 func set_score(val) -> void:
     score = val
-    print("set score: " + str(score))
+    print("set")
 
-func get_score() -> int:
-    return score
+func set_bestScore(val) -> void:
+    bestScore = val
+    emit_signal("best_score_changed", bestScore)

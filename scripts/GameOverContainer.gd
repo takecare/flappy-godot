6cd21e6e571dfcac0f3d1b880fef49136d2ad8f3
takecare <rui@vazteixeira.org>
2021-06-16 15:10:38 +0100

    delay displaying game over screen

--- scripts/GameOverContainer.gd
+++ scripts/GameOverContainer.gd
@@ -4,7 +4,10 @@ func _ready() -> void:
   pass
 
 func _on_bird_hit() -> void:
-  show()
+  showWithDelay()
 
 func _on_bird_grounded() -> void:
-  show()
+  showWithDelay()
+
+func showWithDelay() -> void:
+  get_node("AnimationPlayer").play("Show")

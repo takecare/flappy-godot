7b5791aacd8f8e7d5ebca3ac7d5e35383214a99b
takecare <rui@vazteixeira.org>
2021-06-19 16:22:39 +0100

    fade game over buttons in

--- scripts/GameOverButtons.gd
+++ scripts/GameOverButtons.gd
@@ -3,7 +3,12 @@ extends HBoxContainer
 export(NodePath) var lastScorePath = null
 onready var lastScore: Node = get_node(lastScorePath) if lastScorePath != null else get_node("../ScorePanelContainer/LastScore")
 
+export(NodePath) var animationPlayerPath = null
+onready var animationPlayer: AnimationPlayer = get_node(animationPlayerPath) if animationPlayerPath != null else get_node("../AnimationPlayer")
+
 func _ready():
-  hide()
-  lastScore.connect("last_score_count_finished", self, "show")
+  var _result = lastScore.connect("last_score_count_finished", self, "_fadeIn")
   # TODO: fade in as LastScore is finished with the counting animation
+
+func _fadeIn():
+  animationPlayer.play("Show Buttons")

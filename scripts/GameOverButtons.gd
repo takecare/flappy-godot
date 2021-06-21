f365a5be0eb482230b3c17148d2a0a20ae507eca
takecare <rui@vazteixeira.org>
2021-06-21 23:00:30 +0100

    hide game over buttons on start

--- scripts/GameOverButtons.gd
+++ scripts/GameOverButtons.gd
@@ -7,8 +7,8 @@ export(NodePath) var animationPlayerPath = null
 onready var animationPlayer: AnimationPlayer = get_node(animationPlayerPath) if animationPlayerPath != null else get_node("../AnimationPlayer")
 
 func _ready():
+  modulate = Color(1, 1, 1, 0)
   var _result = lastScore.connect("last_score_count_finished", self, "_fadeIn")
-  # TODO: fade in as LastScore is finished with the counting animation
 
 func _fadeIn():
   animationPlayer.play("Show Buttons")
